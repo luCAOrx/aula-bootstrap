1d71328c61cb382d7e3d25eeb858e4df1121822a
luCAOrx <lucaorx@protonmail.com>
2020-10-17 14:39:24 -0300

    first commit

---
+++ js/cadastro.js
@@ -0,0 +1,133 @@
+function cadastrar() {
+
+    let formulario = [];
+   
+    let id = document.getElementById('id').value;
+
+
+    let nome = validaNome(document.getElementById('nome').value);
+    if (nome == false) {
+        alert('Insira um nome válido!');
+        document.getElementById('nome').focus();
+        return;
+    }
+
+    let fone = document.getElementById('fone').value;
+
+    let cidade = document.getElementById('cidade').value;
+
+    let sexo = '';
+    if (document.getElementById('masculino').checked) {
+        sexo = 'Masculino';
+    } else if (document.getElementById('feminino').checked) {
+        sexo = 'Feminino';
+    } else {
+        sexo = 'Prefiro não comentar';
+    }
+
+    insereNaTabela(id, nome, fone, sexo, cidade);
+
+    limaprFormulario();
+
+}
+
+function limaprFormulario() {
+    document.getElementById('id').value = 0;
+    document.getElementById('nome').value = '';
+    document.getElementById('fone').value = '';
+    document.getElementById('cidade').value = 'Selecione';
+    document.getElementById('masculino').checked = true;
+    document.getElementById('nome').focus();
+}
+
+
+function validaNome(nome) {
+    let texto = nome.trim().toUpperCase();
+    for (let i = 0; i < texto.length; i++) {
+        if (ehNumero(texto[i])) {
+            return false;
+        }
+    }
+    return texto;
+}
+
+function ehNumero(numero) {
+return !isNaN(numero);
+}
+
+function insereNaTabela(id, nome, fone, sexo, cidade) {
+    let tabela = document.getElementById('lista-de-contatos').getElementsByTagName('tbody')[0];
+    
+    let ultimaLinha = tabela.rows.length;
+
+    if (id == 0) {
+        let linha = tabela.insertRow(ultimaLinha);
+
+        let campoId = linha.insertCell(0);
+        let campoNome = linha.insertCell(1);
+        let campoFone = linha.insertCell(2);
+        let campoSexo = linha.insertCell(3);
+        let campoCidade = linha.insertCell(4);
+        let acoes = linha.insertCell(5);
+        
+        campoId.innerHTML = ultimaLinha + 1;
+        campoNome.innerHTML = nome;
+        campoFone.innerHTML = fone;
+        campoSexo.innerHTML = sexo;
+        campoCidade.innerHTML = cidade;
+        acoes.innerHTML = insereBotoesdeAcoes(ultimaLinha + 1);
+    } else {
+        let linha = id - 1;
+        tabela.rows[linha].cells[1].innerHTML = nome;
+        tabela.rows[linha].cells[2].innerHTML = fone;
+        tabela.rows[linha].cells[3].innerHTML = sexo;
+        tabela.rows[linha].cells[4].innerHTML = cidade;
+    }
+
+
+}
+
+function insereBotoesdeAcoes(id) {
+    let botaoEditar = '<button type="button" onclick="buscaContatoPeloId(' + id + ')" class="btn btn-primary btn-sm">';
+    botaoEditar += '<i class="fas fa-pencil-alt"></i>';
+    botaoEditar += '</button>';
+
+    let botaoRemover = '<button class="btn btn-danger btn-sm">';
+    botaoRemover += '<i class="fas fa-trash-alt"></i>';
+    botaoRemover += '</button>';
+    
+    return botaoEditar + botaoRemover;
+}
+
+function buscaContatoPeloId(id) {
+    let body = document.getElementById('lista-de-contatos').getElementsByTagName('tbody')[0];
+    let quantidadeDeLinhas = body.rows.length;
+    for (let i = 0; i < quantidadeDeLinhas; i++) {
+        if (body.rows[i].cells[0].innerHTML == id) {
+            let inputId = document.getElementById('id');
+            inputId.value = body.rows[i].cells[0].innerHTML;
+
+            let inputNome = document.getElementById('nome');
+            inputNome.value = body.rows[i].cells[1].innerHTML;
+
+            let inputFone = document.getElementById('fone');
+            inputFone.value = body.rows[i].cells[2].innerHTML;
+
+            let sexo = body.rows[i].cells[3].innerHTML.toLocaleLowerCase();
+            if (sexo == 'masculino') {
+                document.getElementById('masculino').checked = true;
+            } else if (sexo == 'feminino') {
+                document.getElementById('feminino').checked = true;
+            }  else {
+                document.getElementById('outros').checked = true;
+            }
+
+            let selectCidade = document.getElementById('cidade');
+            selectCidade.value = body.rows[i].cells[4].innerHTML;
+
+            inputNome.focus();
+
+            return;
+        }
+    }
+}
